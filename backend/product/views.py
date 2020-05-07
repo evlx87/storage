4ba411f7e04007f68f8c4dc815da117f09cade8e
Sergey Semyonov <evlx@mail.ru>
2020-05-07 23:16:16 +0300

    now images upload to database correctly

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,4 +1,9 @@
+import csv
+
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
+from backend.product.forms import ProductForm
 from backend.product.models import Product
 
 
@@ -9,3 +14,25 @@ def main(request):
     }
     return render(request, 'index.html', context)
 
+
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = ProductForm()
+
+    context = {
+        'page_title': 'Добавление нового товара',
+        'form': form
+    }
+    return render(request, 'add_product.html', context)
+
+
+def file_output(request):
+    context = {
+        'page_title': 'Подготовка файла CSV',
+    }
+    return render(request, 'output_form.html', context)
